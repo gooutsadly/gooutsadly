be5f6c95b599c6d09e253754ac1e27097cf78581
gooutsadly <gooutsadly@gmail.com>
2021-02-18 02:50:50 +0800

    feat: add tabbar

--- src/Tabs.js
+++ src/Tabs.js
@@ -3,6 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeStackScreen from './HomeStack';
 import {Text, View} from 'react-native';
 // import Icon from 'react-native-vector-icons/dist/Entypo';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,25 +19,35 @@ const Tabs = ({descriptors, navigation}) => {
   return (
     <Tab.Navigator
       screenOptions={{tabBarVisible: false}}
-      // screenOptions={({route}) => ({
-      //   tabBarIcon: ({focused, color, size}) => {
-      //     let iconName;
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
 
-      //     switch (route.name) {
-      //       case 'Home':
-      //         iconName = focused ? 'home' : 'home';
-      //         break;
-      //       case 'Settings':
-      //         iconName = focused ? 'home' : 'home';
-      //         break;
-      //       default:
-      //         iconName = focused ? 'home' : 'home';
-      //     }
+          switch (route.name) {
+            case '主頁':
+              iconName = focused ? 'home' : 'home-outline';
+              break;
+            case '收件箱':
+              iconName = focused ? 'mail' : 'mail-outline';
+              break;
+              case '設定':
+              iconName = focused ? 'settings' : 'settings-outline';
+              break;
+            default:
+              iconName = focused ? 'home' : 'home-outline';
+          }
 
-      //     // You can return any component that you like here!
-      //     return <Icon name={iconName} size={size} color={color} />;
-      //   },
-      // })}
+          // You can return any component that you like here!
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#12b187',
+        inactiveTintColor: 'gray',
+        style: { height: 80 },
+        iconStyle: { marginTop: 16 },
+        labelStyle: { marginBottom: 16 }
+      }}
     >
       <Tab.Screen name="主頁" component={HomeStackScreen} />
       <Tab.Screen name="收件箱" component={SettingsScreen} />
